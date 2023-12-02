a7ce9c13b935341b70db74fc1a32a1e04ae9d55b
亚 陈 <2227237581@qq.com>
2023-12-02 21:55:45 +0800

    添加了参数页面

--- YaMoSignalTools/Pages/SignalMakerPage.xaml.cs
+++ YaMoSignalTools/Pages/SignalMakerPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScottPlot;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -35,6 +36,39 @@ namespace YaMoSignalTools.Pages
             DataList.Add("周期信号");
             DataList.Add("噪声信号");
             DataList.Add("混合信号");
+
+            // sample data
+            double[] xs = DataGen.Consecutive(51);
+            double[] sin = DataGen.Sin(51);
+            double[] cos = DataGen.Cos(51);
+
+            // plot the data
+            WpfPlot1.Plot.AddScatter(xs, sin);
+            WpfPlot2.Plot.AddScatter(xs, cos);
+            WpfPlot3.Plot.AddScatter(xs, cos);
+
+            // customize the axis labels
+            WpfPlot1.Plot.XLabel("x");
+            WpfPlot1.Plot.YLabel("y");
+
+            WpfPlot2.Plot.XLabel("x");
+            WpfPlot2.Plot.YLabel("y");
+
+            WpfPlot3.Plot.XLabel("x");
+            WpfPlot3.Plot.YLabel("y");
+
+            WpfPlot1.Refresh();
+            WpfPlot2.Refresh();
+            WpfPlot3.Refresh();
+
+            PeriodSignalParamPage periodSignalParamPage = new PeriodSignalParamPage();
+            if (periodSignalParamPage != null)
+            {
+                SignalParamPresenter.Content = new Frame()
+                {
+                    Content = periodSignalParamPage
+                };
+            }
         }
     }
 }
