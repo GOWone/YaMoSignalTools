5c4b5682474d7b5ed9b4ac0082807b8466a1f9b0
亚 陈 <2227237581@qq.com>
2023-12-20 23:32:53 +0800

    配置了信号生成器结果信息模型、重新设计了参数设置页面样式、修复了ListView滚动异常

---
+++ YaMoSignalTools/Models/SigMakerResInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YaMoSignalTools.Models
+{
+    public class SigMakerResInfo
+    {
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		private string flag;
+
+		public string Flag
+		{
+			get { return flag; }
+			set { flag = value; }
+		}
+
+
+		private string remark;
+
+		public string Remark
+		{
+			get { return remark; }
+			set { remark = value; }
+		}
+	}
+}
--- YaMoSignalTools/Pages/PeriodSignalParamPage.xaml.cs
+++ YaMoSignalTools/Pages/PeriodSignalParamPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using YaMoSignalTools.Models;
 
 namespace YaMoSignalTools.Pages
 {
@@ -22,10 +23,24 @@ namespace YaMoSignalTools.Pages
     {
         List<string> DataList = new List<string>() { 
             "正弦信号生成器", "余弦信号生成器", "三角波信号生成器", "方波信号生成器", "脉冲波信号生成器", "锯齿波信号生成器"};
+        List<SigMakerResInfo> sigmakerResInfos = new List<SigMakerResInfo>();
         public PeriodSignalParamPage()
         {
             InitializeComponent();
+            InitInfo();
+            ResInfoList.ItemsSource = sigmakerResInfos;
             PeriodSigSelector.ItemsSource = DataList;
+        }
+
+        private void InitInfo()
+        {
+            sigmakerResInfos.Clear();
+            sigmakerResInfos.Add(new SigMakerResInfo() { Name = "信号类型", Flag = "->", Remark = "周期信号" });
+            sigmakerResInfos.Add(new SigMakerResInfo() { Name = "信号生成器", Flag = "->", Remark = "正弦信号生成器" });
+            sigmakerResInfos.Add(new SigMakerResInfo() { Name="信号长度", Flag = "->", Remark="1024"});
+            sigmakerResInfos.Add(new SigMakerResInfo() { Name="信号频率(Hz)", Flag = "->", Remark ="100"});
+            sigmakerResInfos.Add(new SigMakerResInfo() { Name="最大值", Flag = "->", Remark ="100"});
+            sigmakerResInfos.Add(new SigMakerResInfo() { Name="最小值", Flag = "->", Remark ="-100"});
         }
     }
 }
