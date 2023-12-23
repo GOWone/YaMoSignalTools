048e4d37f9e1f73e6c8e93c4a54db32eec60c3ae
亚 陈 <2227237581@qq.com>
2023-12-23 13:19:15 +0800

    新增QQ邮箱、163邮箱支持

---
+++ YaMoSignalTools/Extensions/Constant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YaMoSignalTools.Extensions
+{
+    public class Constant
+    {
+        #region 邮件服务
+        //163SMTP协议
+        public const string EMAIL_163_SMTPPROT = "smtp.163.com";
+        //QQSMTP协议
+        public const string EMAIL_QQ_SMTPPROT = "smtp.qq.com";
+
+        //163邮箱授权码
+        public const string EMAIL_163_AUTHCODE = "OIDVRYKGLYYPQEXH";
+        //QQ邮箱授权码
+        public const string EMAIL_QQ_AUTHCODE = "ivxwkodxtmujdhif";
+
+        //163发件地址
+        public const string EMAIL_163_SENDADDR = "mchenya@163.com";
+        //163收件地址
+        public const string EMAIL_163_RECEADDR = "yamostudio@163.com";
+
+        //QQ发件地址
+        public const string EMAIL_QQ_SENDADDR = "2227237581@qq.com";
+        //QQ收件地址
+        public const string EMAIL_QQ_RECEADDR = "yamostudio@163.com";
+        #endregion
+
+    }
+}
